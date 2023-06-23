0703186b0291866e9ec54797743231f3bb0c6340
John Sandoval <wedabeast360@gmail.com>
2023-06-22 20:37:29 -0700

    billing, settings

--- components/header.tsx
+++ components/header.tsx
@@ -15,9 +15,7 @@ export function DashboardHeader({
         <h1 className="font-heading text-3xl text-white md:text-4xl">
           {heading}
         </h1>
-        {text && (
-          <p className="text-lg text-muted-foreground text-white">{text}</p>
-        )}
+        {text && <p className="text-lg text-white">{text}</p>}
       </div>
       {children}
     </div>
